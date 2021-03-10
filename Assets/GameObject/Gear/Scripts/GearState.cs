bd7fd6616490bdab6453712af395787a5e3a9021
Ryosuke Suhara <ryosuke.suhara0727@gmail.com>
2021-03-10 10:18:46 +0900

    20210310 tougou

---
+++ Assets/GameObject/Gear/Scripts/GearState.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearState : MonoBehaviour
+{
+    //ギアの状態
+    public enum State
+    {
+        single,
+        adapt
+    }
+    State state = State.single;
+
+    //ギアの力の大きさ
+    [SerializeField]
+    int gearPower;
+
+    //ギアの耐久度
+    [SerializeField]
+    float gearDurability = 100;
+
+    //ギアにダメージを与えるときに使うタイマー用の変数
+    float gearDamegeTimer = 0;
+
+    //ギアが受けている力の大きさを保存する変数
+    float gearReceivePower;
+
+    //つながっているギアの情報を保持しておく変数
+    public GameObject beforeGear;
+    bool beforeGearCheck = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (this.state)
+        {
+            case State.single: Single(); break;
+            case State.adapt:  Adapt();  break;
+        }
+    }
+
+    //歯車の状態がsingleの時呼ばれる関数
+    public void Single()
+    {
+
+    }
+
+    //歯車の状態がadaptの時呼ばれる関数
+    public void Adapt()
+    {
+        GearDamege();
+    }
+
+    //歯車にかかっている力の大きさを受け取る関数
+    public void GearReceivePower(int power)
+    {
+        //かかっている力を登録する
+        this.gearReceivePower = power;
+
+        if(power > this.gearPower && this.gearDamegeTimer <= 0)
+        {
+            this.gearDamegeTimer = 2;
+        }else if(power <= this.gearPower)
+        {
+            this.gearDamegeTimer = 0;
+        }
+    }
+
+    //ギアの耐久度を減らす関数
+    //引数はどれだけオーバーパワーがかかっているか
+    void GearDamege()
+    {
+        //タイマーを減らしていき、０になったらダメージを与える
+        if(this.gearDamegeTimer > 0)
+        {
+            this.gearDamegeTimer -= Time.deltaTime;
+        }
+        else if(this.gearDamegeTimer <= 0)
+        {
+            this.gearDurability -= this.gearReceivePower - this.gearPower;
+            this.gearDamegeTimer = 2;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        //つながっている歯車の情報を持つ
+        if(collision.gameObject.tag == "Gear")
+        {
+            if (this.beforeGearCheck == true)
+            {
+                this.beforeGear = collision.gameObject;
+                this.beforeGearCheck = false;
+            }
+            
+        }
+        
+    }
+
+    public void ChangeState(State next)
+    {
+        //後始末
+        switch (this.state)
+        {
+            case State.single:
+                {
+                    //stateを切り替えた際に後始末が必要ならばここに記述
+                }
+                break;
+            case State.adapt:
+                {
+                    //stateを切り替えた際に後始末が必要ならばここに記述
+
+                    //ダメージタイマーを０にする
+                    this.gearDamegeTimer = 0;
+                }
+                break;
+        }
+
+        //stateの切り替え   
+        this.state = next;
+
+        //初期化処理
+        switch (this.state)
+        {
+            case State.single:
+                {
+                    //stateを切り替えた際に初期化が必要ならばここに記述
+                }
+                break;
+            case State.adapt:
+                {
+                    //stateを切り替えた際に初期化が必要ならばここに記述
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/GameObject/Gear/Scripts/GearTouch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearTouch : MonoBehaviour
+{
+    //半径取得用
+    Bounds bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        Vector3 nearGearPosition = collision.transform.position;
+        Vector3 gearPosition = transform.position;
+
+        float gearVectorX = Mathf.Abs(gearPosition.x - nearGearPosition.x);
+        float gearVectorY = Mathf.Abs(gearPosition.y - nearGearPosition.y);
+        float gearVectorZ = Mathf.Abs(gearPosition.z - nearGearPosition.z);
+
+        Vector3 gearVector = new Vector3(gearVectorX, gearVectorY, gearVectorZ);
+
+        //ギアの位置を移動する
+        transform.position = collision.transform.position + gearVector; 
+    }
+}
