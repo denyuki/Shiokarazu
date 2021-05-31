a28e6129463bf50ffb35aa020475e4c2ec9081ad
Ryosuke Suhara <ryosuke.suhara0727@gmail.com>
2021-05-31 13:27:52 +0900

    20210531 仮統合

--- Assets/GameObject/Factory/Scripts/GearFactoryController.cs
+++ Assets/GameObject/Factory/Scripts/GearFactoryController.cs
@@ -10,6 +10,18 @@ public class GearFactoryController : MonoBehaviour
     float frontNum = 0f;
     float backNum = 1f;
 
+    [SerializeField] GameObject GearOneSwitch;
+    [SerializeField] GameObject GearOneText;
+
+    [SerializeField] GameObject GearTwoSwitch;
+    [SerializeField] GameObject GearTwoText;
+
+    [SerializeField] GameObject GearThreeSwitch;
+    [SerializeField] GameObject GearThreeText;
+
+    int GearSwitchOnSorting = 6;
+    int GearSwitchOffSorting = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +39,14 @@ public class GearFactoryController : MonoBehaviour
     {
         this.spriteRenderer.sortingOrder = 5;
         transform.position = new Vector3(transform.position.x, transform.position.y, this.frontNum);
+
+        this.GearOneSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOnSorting;
+        this.GearTwoSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOnSorting;
+        this.GearThreeSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOnSorting;
+
+        this.GearOneText.SetActive(true);
+        this.GearTwoText.SetActive(true);
+        this.GearThreeText.SetActive(true);
     }
 
     //gearFactoryを後ろ側にする関数
@@ -34,5 +54,13 @@ public class GearFactoryController : MonoBehaviour
     {
         this.spriteRenderer.sortingOrder = 0;
         transform.position = new Vector3(transform.position.x, transform.position.y, this.backNum);
+
+        this.GearOneSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOffSorting;
+        this.GearTwoSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOffSorting;
+        this.GearThreeSwitch.gameObject.GetComponent<SpriteRenderer>().sortingOrder = this.GearSwitchOffSorting;
+
+        this.GearOneText.SetActive(false);
+        this.GearTwoText.SetActive(false);
+        this.GearThreeText.SetActive(false);
     }
 }
